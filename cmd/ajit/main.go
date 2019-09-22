2579595001e9c4a40a3bdd7158b3f96158610265
Kushal Das <mail@kushaldas.in>
2019-09-22 23:15:53 +0530

    Half done code to execute a command

--- cmd/ajit/main.go
+++ cmd/ajit/main.go
@@ -3,10 +3,37 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os/exec"
 
 	"github.com/go-redis/redis/v7"
 	"github.com/kushaldas/bomcapture/pkg/capturing"
 )
+
+func ExecuteCommand() {
+	redisdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
+
+	var command string
+	for {
+
+		rawdata := redisdb.BLPop(0, "execute")
+		// First get the result of the command
+		data, ok := rawdata.Result()
+		if ok == nil {
+			// We unmarshal once to find out the type of the data
+			json.Unmarshal([]byte(data[1]), &command)
+		}
+		// This cmd can be a security risk
+		// TODO: Sanitize the input there.
+		fmt.Println(command)
+		// Execute here below
+		cmd := exec.Command("cowsay", "hello")
+		cmd.Run()
+	}
+}
 
 /*
 Entry point for the executable
@@ -17,6 +44,8 @@ func main() {
 		Password: "",               // no password set
 		DB:       0,                // use default DB
 	})
+
+	go ExecuteCommand()
 
 	for {
 		var typedData []map[string]interface{}
@@ -37,7 +66,7 @@ func main() {
 			redisdb.RPush("dnsqueue", BDNSBytes)
 			json.Unmarshal(BDNSBytes, &DNSData)
 			// TODO: Save to the database
-			fmt.Printf("%#v\n", DNSData)
+			//fmt.Printf("%#v\n", DNSData)
 		}
 
 	}
